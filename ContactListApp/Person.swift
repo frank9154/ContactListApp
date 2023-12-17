f90d27c85ca30f2e21dc2a06f11a26b4a789245d
alexsokolov <frank9154@gmail.com>
2023-12-17 10:42:29 +0300

    Third commit

--- ContactListApp/Person.swift
+++ ContactListApp/Person.swift
@@ -17,8 +17,37 @@ class DataStore {
     
     private var uniqueEmails = Set<String>()
     private var uniquePhones = Set<String>()
-       
-
+    
+    func generateRandomPerson() -> Person {
+        let firstName = firstNames.randomElement() ?? "Unknown"
+        let lastName = lastNames.randomElement() ?? "Unknown"
+        let email = generateUniqueEmail()
+        let phone = generateUniquePhone()
+        return Person(fullName: "\(firstName) \(lastName)", email: email, phone: phone)
+    }
+    
+    private func generateUniqueEmail() -> String {
+        var email = ""
+        repeat {
+            email = "\(randomString(length: 7))@example.com"
+        } while uniqueEmails.contains(email)
+        uniqueEmails.insert(email)
+        return email
+    }
+    
+    private func generateUniquePhone() -> String {
+        var phone = ""
+        repeat {
+            phone = String(format: "+1%010d", Int.random(in: 0...9999999999))
+        } while uniquePhones.contains(phone)
+        uniquePhones.insert(phone)
+        return phone
+    }
+    
+    private func randomString(length: Int) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyz"
+        return String((0..<length).map{ _ in letters.randomElement()!})
+    }
     
 }
 
